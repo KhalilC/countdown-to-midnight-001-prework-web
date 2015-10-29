3a11a73826c784991aa56e0e5a987f85c8e0f52d
KhalilC <klcrumpler@gmail.com>
2015-10-29 00:27:31 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  begin
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end until seconds == 0
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep 5
 end
